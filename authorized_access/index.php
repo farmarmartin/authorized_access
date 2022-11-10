1bb14bf98b612e9c2a0c58dc2ea0c3fc5d8257d1
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-11-10 11:08:47 +0100

    upload

---
+++ authorized_access/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>authorizace</title>
+</head>
+<body>
+    <nav>
+        <button name="register">register</button>
+        <button name="login">login</button>
+    </nav>
+</body>
+</html>
