49a603d0516dd2804c6eea82800f226be632547b
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-11-28 19:12:56 +0100

    upload

---
+++ authorized_access/register/register.php
@@ -0,0 +1,16 @@
+<?php
+include '../dat/dbh.php';
+
+$first = $_POST['name'];
+$last = $_POST['surname'];
+$user = $_POST['username'];
+$passw = sha1($_POST['password']);
+$role = 'user';
+
+
+$insert = "INSERT INTO logins (loginName, loginSurname, loginUsername, loginPassw, rName) VALUES ('$first', '$last', '$user', '$passw', '$role');";
+mysqli_query($connect, $insert);
+
+
+
+header("Location: ../login/index.php");
