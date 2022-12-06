579b3ad71de3175027acc4107cb9091fb5d3588c
Martin Job <90215739+farmarmartin@users.noreply.github.com>
2022-12-06 19:34:29 +0100

    deleted one meme

--- authentication/login/memes.php
+++ authentication/login/memes.php
@@ -7,7 +7,6 @@
     'https://imgix.ranker.com/user_node_img/50100/1001983254/original/it-makes-you-a-programmer-photo-u1?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375',
     'https://miro.medium.com/max/920/0*8FnJljSiIINlhcs_',
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_YfVEZwLPuMYghhW-nnUIENxbK5g64elKgB4afLcYnjA9Pbs8xlNvqHtJzUWIsn0ju78&usqp=CAU',
-    'https://www.digitaltechnologylabs.com/wp-content/uploads/2019/06/01-240x300.jpg',
     'https://pbs.twimg.com/media/Ei1-My3UMAAwJ4a.jpg',
     'https://i.pinimg.com/originals/87/7a/13/877a13c05bd3699376c4124761062a6d.png',
     'https://i.pinimg.com/736x/8c/36/50/8c3650e5343c3289b353125155640fcd.jpg',
